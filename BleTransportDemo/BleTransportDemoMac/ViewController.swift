3439b774b3e8726784ff96bc7202646e7cd9997f
Dante P <dantejpuglisi@gmail.com>
2022-11-01 10:49:49 -0300

    Actioned requested changes

--- BleTransportDemo/BleTransportDemoMac/ViewController.swift
+++ BleTransportDemo/BleTransportDemoMac/ViewController.swift
@@ -34,12 +34,38 @@ class ViewController: NSViewController {
 
     @IBAction func searchButtonTapped(_ sender: Any) {
         searchButton.isEnabled = false
+        BleTransport.shared.bluetoothStateCallback { state in
+            if !self.created {
+                switch state {
+                case .poweredOn:
+                    self.created = true
+                    self.create()
+                case .poweredOff:
+                    self.showBluetoothError(message: "Bluetooth is off, try turning it on.")
+                case .unauthorized:
+                    self.showBluetoothError(message: "Bluetooth is not authorized, please go to Settings and allow Bluetooth access.")
+                case .unsupported:
+                    self.showBluetoothError(message: "Bluetooth is not supported on this device.")
+                default:
+                    break
+                }
+            }
+        }
         BleTransport.shared.bluetoothAvailabilityCallback { availability in
             if !self.created && availability {
                 self.created = true
                 self.create()
             }
         }
+    }
+    
+    func showBluetoothError(message: String) {
+        let alert = NSAlert()
+        alert.messageText = "Bluetooth error"
+        alert.informativeText = message
+        alert.alertStyle = .warning
+        alert.addButton(withTitle: "OK")
+        alert.runModal()
     }
 }
 
